a408b60499334d471e85f139a994ae59647f40bf
Elvira Gizatullina <elvirawed@gmail.com>
2023-02-17 15:12:11 -0800

    initial commit

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var financeLogerPerson = "Elvira";
+console.log(financeLogerPerson);
+var inputs = document.querySelectorAll("input");
+inputs.forEach(function (input) {
+  console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+const financeLogerPerson = "Elvira";
+
+console.log(financeLogerPerson);
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  console.log(input);
+});
